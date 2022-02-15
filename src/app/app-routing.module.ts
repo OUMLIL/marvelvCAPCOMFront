f6c0d6ae3c811e0ecac22166c08259d5d43f2218
Mohamed MAAZOUN <Mohamed.MAAZOUN@etu.uca.fr>
2022-02-15 14:23:42 +0100

    main page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GameComponent } from './components/game/game.component';
+
+const routes: Routes = [
+  { path: 'game', component: GameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../css/bootstrap.min.css', '../css/bootstrap.min.css']
 })
+
 export class AppComponent {
-  title = 'marvelvCAPCOMFront';
+  title = 'Marvel VS CAPCOM';
+  mainSoundStatus = 1;
+  mainAudio = new Audio();
+  pikaAudio = new Audio();
+  width = screen.width;
+  height = screen.height;
+
+
+  constructor(private router: Router) {
+    this.mainSound(this.mainAudio);
+    this.playSound(this.mainAudio,0);
+    this.pikaSound(this.pikaAudio);
+  }
+
+  mainSound(audio: HTMLAudioElement) {
+    audio.src = "../assets/Resources/sound.mp3"
+  }
+  pikaSound(audio: HTMLAudioElement) {
+    audio.src = "../assets/Resources/pika.m4a"
+  }
+
+  playSound(audio: HTMLAudioElement, mainSoundStatus: number) {
+    if(!mainSoundStatus){
+      audio.load();
+      audio.play()
+        .then(() => {
+          this.mainSoundStatus = 1;
+          console.log("sound playing");})
+        .catch(error => {
+          console.log(error);
+        });
+    }else{
+      audio.pause();
+      this.mainSoundStatus =0;
+      console.log("sound paused");
+    }
+
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
 }
