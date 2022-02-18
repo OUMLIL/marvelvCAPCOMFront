6cc245279387b23af56579c03bade2e4ab3cb30a
Mohamed MAAZOUN <Mohamed.MAAZOUN@etu.uca.fr>
2022-02-18 13:47:39 +0100

    get player name

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,17 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
+import {IPlayer} from "../../player";
+import {PlayerService} from "../../player.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
+
 export class GameComponent implements OnInit {
-  alert= "Hey"
+  alert= "";
   title = AppComponent.title;
+  player: IPlayer | undefined;
 
-  constructor() { }
+  constructor() {}
+  /*
+  startGame(playerName: string): void {
+    playerName = playerName.trim();
+    if (playerName) {
+      this.playerService.addPlayer({ Nom: playerName.trim() }).subscribe(
+        objet => {
+          location.replace("/window/" + playerName.trim());
+        },
+        error => {
+          switch (error.status) {
+            case 400:
+              this.alert = "Ce pseudo est déjà utilisé";
+              break;
+            default:
+              this.alert = "Impossible de charger la partie";
+          }
+        }
+      );
+    }
+  }
 
+   */
   ngOnInit(): void {
   }
 
