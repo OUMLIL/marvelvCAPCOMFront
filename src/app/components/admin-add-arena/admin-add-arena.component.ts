ef14ab0fd955a30162bd84ca4168b62d2586f79d
Mohamed MAAZOUN <Mohamed.MAAZOUN@etu.uca.fr>
2022-03-11 11:40:17 +0100

    add-arena DONE

--- src/app/components/admin-add-arena/admin-add-arena.component.ts
+++ src/app/components/admin-add-arena/admin-add-arena.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {IAbility} from "../../models/iability.model";
 import {AbilityService} from "../../services/ability.service";
+import {ArenaService} from "../../services/arena.service";
 
 @Component({
   selector: 'app-admin-add-arena',
@@ -8,64 +9,27 @@ import {AbilityService} from "../../services/ability.service";
   styleUrls: ['./admin-add-arena.component.scss']
 })
 export class AdminAddArenaComponent implements OnInit {
-  CharacterName = "NewCharacter";
+  ArenaName = "MyArena";
 
-  abilities : any
+//https://store-images.s-microsoft.com/image/apps.59174.13690668820466484.8f79044a-e86b-43f3-a112-8eaf5adc0f99.8f029a49-a418-4b1c-82a0-2a62dc5529d3?q=90&w=480&h=270
+  img ='https://i.ibb.co/zbx4tbV/arena-example.jpg'
 
-  imgFront ='https://i.ibb.co/xhrf1KC/m1.png'
-  imgBack ="https://i.ibb.co/WymZxSd/m0.png"
-
-  finalUrlIng = "WymZxSd/m0 xhrf1KC/m1" ;
-  Hp = 100;
   selectedDevice: any
 
-  selectedAb1: IAbility = new IAbility();
-  selectedAb2: IAbility = new IAbility();
-  selectedAb3: IAbility = new IAbility();
-  selectedAb4: IAbility = new IAbility();
-  selectedAbilities :IAbility[] = [];
-
   constructor(
-    private abilityService: AbilityService,)
+    private arenaService: ArenaService)
   { }
 
   ngOnInit(): void {
-    this.abilityService.getAllAbilities().subscribe(
-      {
-        next :(data) => this.abilities = data,
-        complete: () => {
-          this.selectedAb1 = this.abilities[0]
-          this.selectedAb2 = this.abilities[1]
-          this.selectedAb3 = this.abilities[2]
-          this.selectedAb4 = this.abilities[3]
-          console.log(this.selectedAb1)
-        }
-      });
   }
 
-  chooseUrlFront(urlFront: string){
-    this.imgFront = urlFront;
+  chooseUrl(url: string){
+    this.img = url;
   }
 
-  chooseUrlBack(urlBack: string){
-    this.imgBack = urlBack;
-  }
-
-  addChar(){
-    this.finalUrlIng =  this.finalUrlIng.replace(
-      this.finalUrlIng.split(" ",3)[1],
-      this.imgFront.replace("https://i.ibb.co/", "").replace(".png","")
-    ).replace(
-      this.finalUrlIng.split(" ",3)[0],
-      this.imgBack.replace("https://i.ibb.co/", "").replace(".png","")
-    )
-    console.log(this.CharacterName)
-    console.log(this.selectedAb1)
-    console.log(this.selectedAb2)
-    console.log(this.selectedAb3)
-    console.log(this.selectedAb4)
-    console.log(this.finalUrlIng)
-    console.log(this.Hp)
+  addArena(){
+    console.log(this.img)
+    console.log(this.ArenaName)
   }
 
 }
